0cf3d12e0b66f45362a1618379be979d6e10b99f
gmenke54 <menke.gw@gmail.com>
2021-12-14 14:54:05 -0800

    depolyed on grants

--- db/index.js
+++ db/index.js
@@ -1,14 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/ridesDatabase';
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/ridesDatabase')
+  .connect(dbUrl)
   .then(() => {
-    console.log('Successfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB.');
   })
   .catch((e) => {
-    console.error('Connection error', e.message)
-  })
-// mongoose.set('debug', true)
-const db = mongoose.connection
+    console.error('Connection error', e.message);
+  });
+mongoose.set('debug', true);
+const db = mongoose.connection;
 
-module.exports = db
+module.exports = db;

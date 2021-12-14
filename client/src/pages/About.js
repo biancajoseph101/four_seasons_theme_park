6bce517311b34627e9e78c830b533aa540591c86
Abidur Rahman Dipta <rahmanabid083@gmail.com>
2021-12-14 15:46:37 -0500

    added style

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
+    <div className='about'>
       <h3>
         Welcome to Baggland, where you can bag on the rides you hate at any
         amusement park in the world! With Baggland, you will know the ride you
         are about to go on sucks before you go on it!
       </h3>
+      <div className='our-about'>
+      <h3>This theme park was created by Bianca, Grant, Gihae and Abidur</h3>
+      <img  src= 'https://cdn-icons-png.flaticon.com/512/174/174857.png' />
+      <div className='links'>
+        <h6><a href="https://www.linkedin.com/in/biancaclairejoseph/">Bianca</a></h6>
+        <h6><a href="https://www.linkedin.com/in/grantmenke/">Grant</a></h6>
+        <h6><a href="https://www.linkedin.com/in/gihaec/">Gihae</a></h6>
+        <h6><a href="https://www.linkedin.com/in/abidurrahmandipta">Abidur</a></h6>
+      </div>
+      </div>
     </div>
   );
 };

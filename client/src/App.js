22cbd084eee251035e2ae22a1b9b788cc27bd307
gmenke54 <menke.gw@gmail.com>
2021-12-14 09:22:17 -0800

    added nav bar and about page

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,22 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import RideDetails from './pages/RideDetails';
+import Nav from './components/Nav';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <Nav />
+      </header>
       <Switch>
         <Route exact path="/" component={(props) => <Home {...props} />} />
         <Route
           path="/rides/details/:rideId"
           render={(props) => <RideDetails {...props} />}
         />
+        <Route exact path="/about" component={About} />
       </Switch>
     </div>
   );
---
+++ client/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h3>
+        Welcome to Baggland, where you can bag on the rides you hate at any
+        amusement park in the world! With Baggland, you will know the ride you
+        are about to go on sucks before you go on it!
+      </h3>
+    </div>
+  );
+};
+
+export default About;

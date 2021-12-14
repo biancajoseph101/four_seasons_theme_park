60089bbd0f306b9488db6cf1dbfafc2a04a91449
gmenke54 <menke.gw@gmail.com>
2021-12-13 16:31:52 -0800

    mapped through rides on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import IntensityCard from '../components/IntensityCard';
+import RideDetails from '../pages/RideDetails';
+import axios from 'axios';
 
 export default function Home() {
+  const [rides, setRides] = useState([]);
+
+  useEffect(() => {
+    getRides();
+  }, []);
+  const getRides = async () => {
+    const res = await axios.get(`http://localhost:3001/api/rides`);
+    setRides(res.data.rides);
+    console.log(res.data.rides);
+    console.log(res.data.rides[1].url);
+  };
   return (
     <div>
-      <IntensityCard name="Thrill-Seekers" />
-      <IntensityCard name="Family-Friendly" />
-      <IntensityCard name="Toddlers" />
+      {rides.map((ride) => {
+        {
+          return (
+            <RideDetails
+              name={ride.name}
+              image={ride.url}
+              description={ride.description}
+            />
+          );
+        }
+      })}
     </div>
   );
 }

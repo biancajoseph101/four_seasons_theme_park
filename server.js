df1aa9b05df7ffa21f6b241697b5a1c250a5db4e
Abidur Rahman Dipta <rahmanabid083@gmail.com>
2021-12-14 16:45:04 -0500

    Done adding heroku and database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const routes = require('./routes');
 const db = require('./db');
@@ -19,5 +20,12 @@ app.use(logger('dev'))
 app.use('/api', routes);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    })
+  }
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
